d17c5c915f73a5351572b92e5c282582afe588a8
rightburrow <rightburrowyt@gmail.com>
2024-11-15 23:55:47 -0600

    new file:   .gitignore 	modified:   index.js

--- index.js
+++ index.js
@@ -8,5 +8,4 @@ const app = express();
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
